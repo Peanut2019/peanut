d9a357fabaa47e8eb11e3fae4db48377a4938f17
Lucifer_yan <53885749+yanzhang417@users.noreply.github.com>
2019-12-24 08:59:54 +0800

    Update zuke.py

--- Peanut/house/dandan/zuke.py
+++ Peanut/house/dandan/zuke.py
@@ -50,31 +50,33 @@ print(new_home_url)
 def home_message():
     for i in new_home_url:
         for b in i:
-            print(url+b)
+#             print(url+b)
             resa = requests.get(url+b)
             home_mess = etree.HTML(resa.text,etree.HTMLParser())
-            district = home_mess.xpath("//span[@class='c-333']/text()")[7]
+            district = home_mess.xpath("//span[@class='c-333']/text()")[7]#房屋所在区
             district = ' '.join(district)
             print(district)
-            house = home_mess.xpath("//span[@class='c-333 left']/text()")
+            house = home_mess.xpath("//span[@class='c-333 left']/text()")#房屋所在位置
             house = ' '.join(house)
             print(house)
-            monthly = home_mess.xpath("//span[@class='f26 bold']/text()")
+            monthly = home_mess.xpath("//span[@class='f26 bold']/text()")#月租
             monthly = ' '.join(monthly)+'元/月'
             print(monthly)
-            house_info = home_mess.xpath("//span[@class='c-333']/text()")
+            house_info = home_mess.xpath("//span[@class='c-333']/text()")#房屋信息
             house_info = ' '.join(house_info)
             print(house_info)
-            agent = home_mess.xpath("//p[@class='mt4 f18 c-333']/text()")
+            agent = home_mess.xpath("//p[@class='mt4 f18 c-333']/text()")#房地产经纪人
             agent_phone = home_mess.xpath("//span[@class='f16 c-ff5555']/text()")
             agent = ' '.join(agent)+':'+' '.join(agent_phone)
             print(agent)
-            imgs = home_mess.xpath("//img[@class='zooming-switch']/@src")
+            imgs = home_mess.xpath("//img[@class='zooming-switch']/@src")#房屋图片展示
             imgs = ' '.join(imgs)
             print(imgs)
-            traffic = home_mess.xpath("//div[@class='plr20 c-333 f14']/text()")
+            traffic = home_mess.xpath("//div[@class='plr20 c-333 f14']/text()")#房屋所在地周围环境
             traffic = ' '.join(traffic)
             print(traffic)
+            property_fee = ' '.join('无物业费!')
+            print(property_fee)
 
 home_message()
 
